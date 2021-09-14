fa1888c11182ff2a5662a77b8fe90e8d9914aa76
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2021-09-14 21:02:21 +0400

    Add new method

--- src/services/weather.data.service.ts
+++ src/services/weather.data.service.ts
@@ -1,7 +1,13 @@
 export default class WeatherService {
-    public static getWeather(latitude: number, longitude: number) {
+    public static getCurrentWeather(latitude: number, longitude: number) {
         return fetch(
             `${process.env.REACT_APP_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=d8ac7f1d840b57650c8d3317209350e7`,
+        );
+    }
+
+    public static getForecastWeather(latitude: number, longitude: number) {
+        return fetch(
+            `${process.env.REACT_APP_API_URL}/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&APPID=d8ac7f1d840b57650c8d3317209350e7`,
         );
     }
 }
