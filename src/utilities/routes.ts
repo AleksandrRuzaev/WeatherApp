e54f6484249ad8ba5641dee4e9e357d15765d4f5
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2021-12-25 22:51:11 +0400

    Add router

---
+++ src/utilities/routes.ts
@@ -0,0 +1,10 @@
+export const routes = {
+    todayTomorrow: {
+        label: 'Today/Tomorrow',
+        route: 'today-tomorrow',
+    },
+    fiveDays: {
+        label: '5-Days Forecast',
+        route: '5-days',
+    },
+};
