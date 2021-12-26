aa28801b85cb3f43095a88da78273110762f8e6b
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2021-12-26 17:16:39 +0400

    Add styled components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 import './App.scss';
 import { Content } from './content';
 import { Header } from './header/header';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { routes } from '../utilities/routes';
 
 const App: React.FC = () => {
     return (
         <>
+            <Router>
+                <Routes>
+                    <Route path={routes.todayTomorrow.route}>
+                        <></>
+                    </Route>
+                    <Route path={routes.fiveDays.route}>
+                        <></>
+                    </Route>
+                    <Route path="/">
+                        <></>
+                    </Route>
+                </Routes>
+            </Router>
             <Header />
             <Content />
         </>
